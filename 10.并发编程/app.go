72b458c8c9c4d638e8679f4b79a88f25728894ac
unliar <370732889@qq.com>
2018-06-28 02:25:00 +0800

    feat: 并发章节

---
+++ 10.并发编程/app.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/*
+ 并发编程
+
+1. 多进程
+
+ 开销最大的模式,系统层面的基本模式
+
+2. 多线程
+
+ 使用最多的一种方式
+
+3. 基于回调的异步io
+
+ 事件驱动使用异步IO 高并发的模式下会消耗cpu资源和内存
+
+4. 协程
+
+ 不需要操作系统来进行调度,系统开销小,寄存于线程中
+
+*/
+
+func Count(ch chan int) {
+	fmt.Println("counting--->")
+	ch <- 1
+}
+
+func main() {
+
+	chs := make([]chan int, 10)
+
+	for i := 0; i < 10; i++ {
+		chs[i] = make(chan int)
+		go Count(chs[i])
+	}
+
+	for _, ch := range chs {
+		<-ch
+	}
+}
