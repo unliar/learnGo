959c04c46cceac2a49d6b2d44a381fbd90d8b716
blessyou <370732899@.com>
2018-08-12 00:20:43 +0800

    feat: 添加shell脚本

---
+++ examples/go-micro-account/start.sh
@@ -0,0 +1,20 @@
+CONSUL=192.168.31.156:8500
+
+if [ $# == 1 ] && [ $1 == "dev" ]
+    then
+        echo "start develop mode"
+        read -p "输入consul ip:" -t 30 ip
+        if [ $ip ] 
+            then
+                echo "当前不为空为空，设置consul-->$ip" 
+                CONSUL=$ip
+        else
+            echo "当前ip为空，使用默认值$CONSUL"
+        fi
+else
+    echo "start production mode"
+    CONSUL=$1
+fi
+
+echo "当前注册中心ip---->$CONSUL"
+CONSUL_HTTP_ADDR=$CONSUL go run main.go  --registry=consul --registry_address=$CONSUL --selector=cache --server=grpc --client=grpc
