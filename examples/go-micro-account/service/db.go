63752f7119969654adbbec197b82a7d2191d4040
blessyou <370732899@.com>
2018-08-12 05:24:46 +0800

    feat: 接入mysql

---
+++ examples/go-micro-account/service/db.go
@@ -0,0 +1,53 @@
+package service
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var DB *gorm.DB
+
+type UserBase struct {
+	gorm.Model
+	LoginName  string `gorm:"unique";NOT NULL;"`
+	IDC        string `gorm:"DEFAULT:'';"`
+	Nickname   string `gorm:"UNIQUE";NOT NULL"`
+	Age        int64  `gorm:"DEFAULT:0"`
+	Male       string `gorm:"size:6"`
+	Avatar     string `gorm:"size:255"`
+	Location   string `gorm:"DEFAULT:'shenzhen'"`
+	Profession string `gorm:"DEFAULT:''"`
+	Status     int64  `gorm:"DEFAULT:1"`
+}
+
+type UserContact struct {
+	gorm.Model
+	UID      int64  `gorm:"unique";`
+	Email    string `gorm:"DEFAULT:''"`
+	Phone    string `gorm:"DEFAULT:''"`
+	WeChatId string `gorm:"DEFAULT:''"`
+	WeiBoId  string `gorm:"DEFAULT:''"`
+	QQId     string `gorm:"DEFAULT:''"`
+}
+
+func init() {
+	fmt.Println("db start init")
+	var err error
+	DB, err = gorm.Open("mysql", "root:MySQL19930224@tcp(127.0.0.1:3306)/go-server?parseTime=true")
+	DB.LogMode(true)
+	if !DB.HasTable(&UserBase{}) {
+		fmt.Println("db UserBase need to create")
+		err = DB.Set("gorm:table_options", "ENGINE=InnoDB").
+			CreateTable(&UserBase{}).Error
+	}
+	if !DB.HasTable(&UserContact{}) {
+		fmt.Println("db UserContact need to create")
+		err = DB.Set("gorm:table_options", "ENGINE=InnoDB").
+			CreateTable(&UserContact{}).Error
+	}
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("db init success")
+}
--- examples/go-micro-account/service/service.go
+++ examples/go-micro-account/service/service.go
@@ -3,26 +3,31 @@ package service
 import "context"
 import (
 	proto "learnGo/examples/go-micro-account/proto"
-	"fmt"
 )
 
 type Account struct {
 }
 
+// GetUserInfo 是用来处理相关用户信息的接口
 func (a *Account) GetUserInfo(ctx context.Context, req *proto.UserId, rsp *proto.UserInfo) error {
-	fmt.Println(rsp.Contact)
-	uc:=proto.UserContact{}
-	ub:=proto.UserBase{}
-	uc.Phone="2222222"
-	rsp.Base=&ub
-	rsp.Contact=&uc
+	uc := proto.UserContact{}
+	ub := proto.UserBase{}
+	var duc UserContact
+	DB.Find(&duc, "uid = ? ", req.UID).First(&duc)
+	uc.Phone, uc.Email, uc.WeiBoId, uc.WeChatId, uc.Id = duc.Phone, duc.Email, duc.WeiBoId, duc.WeChatId, duc.UID
+	rsp.Base = &ub
+	rsp.Contact = &uc
+
 	return nil
 }
 
+// GetUserBase 是用来获取用户基础信息的接口
 func (a *Account) GetUserBase(ctx context.Context, req *proto.UserId, rsp *proto.UserBase) error {
 	rsp.Nickname = "qq"
 	return nil
 }
+
+// GetUserContact 是用来获取用户联络信息的接口
 func (a *Account) GetUserContact(ctx context.Context, req *proto.UserId, rsp *proto.UserContact) error {
 	rsp.Phone = "99999"
 	return nil
