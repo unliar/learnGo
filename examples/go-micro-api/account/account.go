b91b98d131cf40a3defd538d18bb7385c0ffdf3a
unliar <370732889@qq.com>
2018-08-25 15:07:14 +0800

    refactor: 设置api服务为固定端口,添加api服务的健康检测

--- examples/go-micro-api/account/account.go
+++ examples/go-micro-api/account/account.go
@@ -2,11 +2,11 @@ package account
 
 import (
 	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/micro/go-micro/client"
 	ASV "github.com/unliar/proto/account"
 	"strconv"
-	"fmt"
 )
 
 var (
@@ -20,9 +20,9 @@ func init() {
 func GetUserBase(c *gin.Context) {
 	var err error
 	uid := c.Param("uid")
-	UID,err:= strconv.ParseInt(uid,10,64)
+	UID, err := strconv.ParseInt(uid, 10, 64)
 	resp, err := AccountSVService.GetUserBase(context.TODO(), &ASV.UserId{
-		UID:UID,
+		UID: UID,
 	})
 	fmt.Println(resp)
 	if err != nil {
@@ -30,4 +30,11 @@ func GetUserBase(c *gin.Context) {
 		return
 	}
 	c.JSON(200, resp)
+}
+
+func GetHealthStatus(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"status":  200,
+		"message": "api server ok",
+	})
 }
--- examples/go-micro-api/app.go
+++ examples/go-micro-api/app.go
@@ -3,29 +3,30 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/micro/go-web"
-	"log"
 	ac "learnGo/examples/go-micro-api/account"
+	"log"
 )
 
 func main() {
 	var err error
-	
+
 	service := web.NewService(
 		web.Name("unliar-restful-api"),
+		// 端口设置 否则是随机端口
+		web.Address(":8088"),
+		web.Version("beta-1.1.1"),
 	)
-	
+
 	service.Init()
 
 	r := gin.Default()
-	
-	
-	
+	r.GET("/api/health", ac.GetHealthStatus)
 	r.GET("/api/user/:uid", ac.GetUserBase)
-	
+
 	service.Handle("/", r)
-	
+
 	err = service.Run()
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
