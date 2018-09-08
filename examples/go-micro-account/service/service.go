fabf1003c87b7109af26b8cb5e0b41fe8557bf8a
unliar <370732889@qq.com>
2018-09-08 13:09:35 +0800

    refactor: 添加jwt生成token方法

--- examples/go-micro-account/service/service.go
+++ examples/go-micro-account/service/service.go
@@ -2,11 +2,24 @@ package service
 
 import "context"
 import (
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
 	proto "github.com/unliar/proto/account"
 )
 
+// SignKey 用于token签名
+var SignKey = []byte("hikey")
+
 // Account 账户模块
 type Account struct {
+}
+
+// Payload 是用来发token的
+type Payload struct {
+	UID    int64 `json:"uid"`
+	Status int64 `json:"userStatus"`
+	jwt.StandardClaims
 }
 
 // GetUserInfo 是用来获取用户信息的接口
@@ -28,8 +41,20 @@ func (a *Account) UpdateUserInfo(ctx context.Context, req *proto.UserInfo, rsp *
 
 // GetToken 是用来获取合法token的接口
 func (a *Account) GetToken(ctx context.Context, req *proto.UserInfo, rsp *proto.TokenMessage) error {
-
-	return nil
+	claims := Payload{
+		req.GetId(),
+		req.GetStatus(),
+		jwt.StandardClaims{
+			Issuer:    "accountSrv",
+			Subject:   "AccountToken",
+			IssuedAt:  time.Now().Unix(),
+			ExpiresAt: time.Now().Unix() + 70000,
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(SignKey)
+	rsp.Token = signedToken
+	return err
 }
 
 // CheckToken 是用来检测用户token
