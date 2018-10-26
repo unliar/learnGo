5c4834189c403544399070617902f9bd2f874287
unliar <370732889@qq.com>
2018-10-27 03:09:59 +0800

    feat: add wrapCall and wrapHandler

--- examples/go-micro-account/app.go
+++ examples/go-micro-account/app.go
@@ -2,24 +2,29 @@ package main
 
 import (
 	"fmt"
-	SV "learnGo/examples/go-micro-account/service"
-	"time"
-
 	"github.com/micro/go-micro"
 	proto "github.com/unliar/proto/account"
+	SV "learnGo/examples/go-micro-account/service"
+	"learnGo/examples/go-micro-account/utils"
+	"time"
 )
 
 func main() {
-	var err error
 	service := micro.NewService(
 		micro.Name("unliar-account"),
 		micro.Version("beta-1.1.1"),
 		micro.RegisterInterval(time.Second*15),
 		micro.RegisterTTL(time.Second*60),
+		micro.WrapCall(utils.MicroWrapCall),
+		micro.WrapHandler(utils.MicroWrapHandler),
 	)
+
 	service.Init()
+
 	proto.RegisterAccountSVHandler(service.Server(), new(SV.Account))
-	err = service.Run()
+
+	err := service.Run()
+
 	if err != nil {
 		fmt.Println(err)
 	}
--- examples/go-micro-account/utils/utils.go
+++ examples/go-micro-account/utils/utils.go
@@ -1,12 +1,57 @@
 package utils
 
 import (
+	"context"
 	"crypto/md5"
+	"encoding/json"
 	"fmt"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/server"
+	"time"
 )
 
 // CreateMD5 是用来创建md5值的
 func CreateMD5(s, salt string) string {
 	result := s + salt
 	return fmt.Sprintf("%x", md5.Sum([]byte(result)))
+}
+
+// MicroWrapCall 是用来包装rpc client的
+func MicroWrapCall(c client.CallFunc) client.CallFunc {
+	return func(ctx context.Context, address string, req client.Request, rsp interface{}, opts client.CallOptions) error {
+		t := time.Now()
+		err := c(ctx, address, req, rsp, opts)
+		fmt.Printf("HiWrapCall==> %s -  %s - %s - %v ", address, req.Service(), req.Method(), time.Since(t))
+		return err
+	}
+}
+
+// LoggerInfo 是service的请求信息
+type LoggerInfo struct {
+	Method  interface{} `json:"method"`
+	Payload interface{} `json:"payload"`
+	Service interface{} `json:"service"`
+	Spend   interface{} `json:"spend"`
+	Meta    interface{} `json:"meta"`
+}
+
+// MicroWrapHandler 是用来包装service的
+func MicroWrapHandler(s server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, rsp interface{}) error {
+		t := time.Now()
+		metaData, _ := metadata.FromContext(ctx)
+		err := s(ctx, req, rsp)
+		logger := LoggerInfo{
+			Method:  req.Method(),
+			Payload: req.Request(),
+			Service: req.Service(),
+			Spend:   time.Since(t).String(),
+			Meta:    metaData,
+		}
+		s, _ := json.Marshal(logger)
+
+		fmt.Printf("%s", s)
+		return err
+	}
 }
