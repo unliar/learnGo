421f9710158748e0dbbdd926a28a60ddf30e297a
unliar <370732889@qq.com>
2018-12-06 02:10:27 +0800

    feat:快排

--- turial/11.算法图解/app.go
+++ turial/11.算法图解/app.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Sum(arr []int64) int64 {
 	if len(arr) == 1 {
@@ -29,6 +31,27 @@ func Total(arr []int64) int64 {
 	}
 	return 1 + Total(arr[1:])
 }
+
+func Sort(arr []int64) []int64 {
+	if len(arr) < 2 {
+		return arr
+	}
+	i := arr[0]
+	var l []int64
+	var r []int64
+	for _, item := range arr[1:] {
+		if item <= i {
+			l = append(l, item)
+		} else {
+			r = append(r, item)
+		}
+	}
+
+	ll := append(Sort(l), i)
+	tt := append(ll, Sort(r)...)
+	return tt
+
+}
 func main() {
 	a := []int64{1, 2, 3, 4, 5, 7, 8, 8, 9, 90, 434, 34, 3, 43, 43, 4, 34, 3, 43, 4, 34, 3, 43, 4, 3, 4, 34, 3, 4}
 	s := Sum(a)
@@ -37,4 +60,5 @@ func main() {
 	fmt.Println("sum:", s)
 	fmt.Println("max:", m)
 	fmt.Println("total:", t)
+	fmt.Println(Sort(a))
 }
